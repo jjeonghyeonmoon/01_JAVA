c5fe9b7f6f5e5d5b83dbb3458c3ff36e31e24406
answjdgus72 <answjdgus72@gmail.com>
2024-08-26 15:19:52 +0900

    [24.08.26 수업자료]

---
+++ chap00-test-lecture-source/src/main/java/com/ohgiraffers/test/Application.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.test;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World!\nBye");
+        System.out.println("안녕하세요");
+    }
+}
---
+++ test/src/main/java/App.java
@@ -0,0 +1,14 @@
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("보너스를 포함한 급여 : " + ( 1000000 + 200000) + "원");
+
+        int salary = 1000000;
+        int bonus = 200000;
+
+        System.out.println("보너스를 포함한 급여 : " + (salary + bonus) + "원");
+
+        /* 목차 */
+
+    }
+}
