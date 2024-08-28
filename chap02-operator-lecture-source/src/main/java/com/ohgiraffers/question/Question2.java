8ea4df58cee5e53eb0b4d65a798906f8497c5cf7
answjdgus72 <answjdgus72@gmail.com>
2024-08-28 17:08:32 +0900

    2024-08-28

---
+++ chap02-operator-lecture-source/src/main/java/com/ohgiraffers/question/Question2.java
@@ -0,0 +1,31 @@
+package com.ohgiraffers.question;
+
+public class Question2 {
+
+    public static void main(String[] args) {
+
+        Question2 name = new Question2();
+        name.methodA();
+        name.methodB();
+        name.methodC();
+        name.methodD();
+    }
+
+
+    public void methodA() {
+        System.out.print("은미");
+        System.out.println("님 안녕하세요?");
+    }
+    public void methodB() {
+        System.out.print("정현");
+        System.out.println("님 안녕하세요?");
+    }
+    public void methodC() {
+        System.out.print("승현");
+        System.out.println("님 안녕하세요?");
+    }
+    public void methodD() {
+        System.out.print("이정");
+        System.out.println("님 안녕하세요?");
+    }
+}
---
+++ chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Application2.java
@@ -0,0 +1,37 @@
+package com.ohgiraffers.section01.method;
+
+public class Application2 {
+
+    public static void main(String[] args) {
+
+        /* title. 다른 흐름의 메소드 호출에 대해 이해할 수 있다.*/
+
+        /*index. 1. methodA(), methodB(), methodC() 차례로 작성*/
+        /*index. 2. 작성 후 메소드 호출 확인과 실행 종료 출력 구문을 작성*/
+        /*index. 3. 메인메소드 실행 종료를 확인하기 위한 출력 구문을 작성*/
+        System.out.println("main() 시작됨...");
+
+        /*index. 4. */
+        Application2 app2 = new Application2();
+        app2.methodA();
+        app2.methodB();
+        app2.methodC();
+
+        System.out.println("main() 시작됨...");
+    }
+
+    public void methodA() {
+        System.out.println("methodA 호출됨...");
+        System.out.println("methodA 호출됨...");
+    }
+    public void methodB() {
+        System.out.println("methodB 호출됨...");
+        System.out.println("methodB 호출됨...");
+    }
+
+       //public -> 접근제안자
+    public void methodC() {
+            System.out.println("methodC 호출됨...");
+            System.out.println("methodC 호출됨...");
+    }
+}
