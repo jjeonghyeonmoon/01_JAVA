4cc5185013c539bcad4c819f04a802440ff42e1e
answjdgus72 <answjdgus72@gmail.com>
2024-09-09 10:58:54 +0900

    2024-09-09

---
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section04/constructor/Application.java
@@ -0,0 +1,30 @@
+package com.ohgiraffers.section04.constructor;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        /* title. 생성자 함수가 무엇인지 이해하고 선언 및 호출할 수 있다. */
+
+        /* comment.
+        *   생선자란?
+        *   우리가 지금까지 클래스명 래퍼런스변수명 = new 클래스명 ();
+        *   이렇게 객체를 만들어왔다.
+        *   new 뒤에 클래스명은 사실 생성자(constructor) 라고 불리는 메소드를
+        *   호출하는 구문이다.
+        *   하지만, 우리는 지금까지 생성자를 만든 적이 없는데
+        *   매개변수가 없는 생성자는 compiler 에 의해 자동으로 추가되기 때문에
+        *   우리가 지금까지 작성하지 않고도 사용할 수 잇게 되었다.
+        * */
+
+
+
+
+
+
+
+
+
+    }
+
+}
