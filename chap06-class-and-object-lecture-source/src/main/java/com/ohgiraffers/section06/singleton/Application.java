37ef32dc5222884c8fdbc42e894bcd103d6f6e48
answjdgus72 <answjdgus72@gmail.com>
2024-09-10 10:41:07 +0900

    2024-09-10 [1차]

---
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section06/singleton/Application.java
@@ -0,0 +1,21 @@
+package com.ohgiraffers.section06.singleton;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        /* title. 싱글톤 패턴에 대해 이해하고 이를 구현할 수 있다.*/
+
+        /* comment.
+        *   singleton 패턴이랑 */
+
+
+
+
+
+
+
+
+
+    }
+}
