0c6b155bb0c037fedd46908a226946d5eb692c3c
answjdgus72 <answjdgus72@gmail.com>
2024-09-11 16:18:58 +0900

    2024-09-11

--- chap08-inheritance-lecture-source/src/main/java/com/ohgiraffers/section02/superkeyword/Product.java
+++ chap08-inheritance-lecture-source/src/main/java/com/ohgiraffers/section02/superkeyword/Product.java
@@ -81,4 +81,8 @@ public class Product {
                 ", manufacturingDate=" + manufacturingDate +
                 '}';
     }
+
+
+
+
 }
---
+++ chap09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section01/polymorphism/너구리.java
@@ -0,0 +1,47 @@
+package com.ohgiraffers.section01.polymorphism;
+
+public class 너구리 extends 동물 {
+
+    @Override
+    public void 먹기() {
+        System.out.println("너구리가 너구리라면을 먹습니다.");
+    }
+
+    @Override
+    public void 뛰기() {
+        System.out.println("너구리가 뛰뛰합니다.");
+    }
+
+    @Override
+    public void 울부짖기() {
+        System.out.println("너굴너굴너구리");
+    }
+
+    public void 깨물기(){
+        System.out.println("너구리가 자기 밥그릇을 건드 사람에게 깨물기 공격을 시전합니다...");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
